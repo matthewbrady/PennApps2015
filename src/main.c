348dbac5fa13e991991519af9cdf2ef93b189e7e
ethanweinberger <ethanweinberger@gmail.com>
2015-01-17 02:54:36 -0500

    Added sleep detection
    TODO:

--- src/main.c
+++ src/main.c
@@ -1,10 +1,9 @@
 #include <pebble.h>
 static Window *s_main_window;
 static TextLayer *s_time_layer;
-static int secondCount = 0;
-static int x_accel = 0, y_accel = 0, z_accel = 0;
-static int x_prev = 0, y_prev = 0, z_prev = 0;
-static bool buzz = false;
+static int secondCount = 0; //Keeps track of how many seconds since user has moved
+static int x_accel = 0, y_accel = 0, z_accel = 0; //Keeps track of most recent accelerometer reading
+static int x_prev = 0, y_prev = 0, z_prev = 0;    //2nd most recent accelerometer reading
 
 //Tracks whether the watch is moving or not
 //Sensitivity should be updated after testing
@@ -24,22 +23,11 @@ bool moving() {
 
 }
 
-bool keepBuzzing() {
-  //Checks to see if watch has moved by 2000 units on any axis (user flicks after being woken up)
-  if ((x_accel >= x_prev + 1000) || (x_accel <= x_prev - 1000) ||
-        (y_accel >= y_prev + 1000) || (y_accel <= y_prev - 1000) ||
-        (z_accel >= z_prev + 1000) || (z_accel <= z_prev - 1000)) {
-    //If yes, no need to keep buzzing
-    return false;
-  }
-  else {
-    //If no, keep going
-    return true;
-  }
-}
-
 static void data_handler(AccelData *data, uint32_t num_samples) {
-  // Long lived buffer
+  /*Needed to get the data from the accelerometer
+  The 'data' variable has everything we need and has attributes x y and z
+  'num_samples' is how many sample we want for each cycle of sampling ("We're at 10Hz")
+  Only using one to conserve memory/battery*/
   
   if (x_accel == 0 && y_accel == 0 && z_accel == 0) {
     x_accel = data[0].x;    //If this is the first recording, then just record the first values
@@ -57,36 +45,10 @@ static void data_handler(AccelData *data, uint32_t num_samples) {
     z_accel = data[0].z;
   }
   
-  x_accel = data[0].x;
-  y_accel = data[0].y;
-  z_accel = data[0].z;
-  
 }
 
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_time_layer, "Up pressed!");
-}
-
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_time_layer, "Select pressed!");
-}
-
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_time_layer, "Down pressed!");
-}
-
-static void click_config_provider(void *context) {
-  // Register the ClickHandlers
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
-}
-
-
-//Calls the update function every x seconds based on the units_changed variable
+//Calls the function every x where x is units_changed variable in the init() function (using seconds for now)
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  
-  static char s_buffer[128];
   
   //Handling each second
   if (moving()) {
@@ -98,23 +60,35 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
     //If it has been five seconds, give a warning
     if (secondCount == 5) {
       text_layer_set_text(s_time_layer, "Feeling tired?");
+      secondCount++;
     }
+    //Another warning
     else if (secondCount == 10) {
       text_layer_set_text(s_time_layer, "Asleep?");
+      secondCount++;
     }
+    //Last warning
     else if (secondCount == 20) {
       text_layer_set_text(s_time_layer, "Buzzer coming in 10...");
+      secondCount++;
     }
+    //Start buzzing
     else if (secondCount >= 30) {
-      buzz = true;
-      while (buzz) {
+      text_layer_set_text(s_time_layer, "Wakey wakey!");
+      
+      int counter = 0;
+      while (counter < 10) { //Gives 10 long pulses with 1 second of space between each
         vibes_long_pulse();
-        vibes_short_pulse();
         psleep(1000);
-        buzz = keepBuzzing();
+        counter++;
+        
       }
+      secondCount = 0;
+      psleep(10000); //Sleeps for 10 seconds
     }
-    secondCount++;
+    else {
+      secondCount++;
+    }
   }
 }
 
@@ -132,6 +106,7 @@ static void main_window_load(Window *window) {
   //Sets up the font for the layer
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
   
+  //Adds the layer to the window
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
 }
 
@@ -158,20 +133,13 @@ static void init() {
     .load = main_window_load,
     .unload = main_window_unload
   });
-
   
   window_stack_push(s_main_window, true);
-  //update_time();
-  
 }
 
 static void deinit() {
-  
   window_destroy(s_main_window);
-  
 }
-
-
 
 int main(void) {
 

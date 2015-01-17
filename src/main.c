49b2e5f548244eaad206c6185c75d1c1c40b2b6c
ethanweinberger <ethanweinberger@gmail.com>
2015-01-17 17:43:07 -0500

    Icon added

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <pebble.h>
-  
-#define SLEEP_AFTER_MOVE_TIME 5000
+
+#define NUM_MENU_SECTIONS 1
+#define NUM_MENU_ITEMS 2
 
 enum App_State {
   awake,
@@ -10,6 +11,11 @@ enum App_State {
   ringing    
 };
   
+static Window *s_menu_window;
+static SimpleMenuLayer *simple_menu_layer;
+static SimpleMenuSection menu_sections[NUM_MENU_SECTIONS];
+static SimpleMenuItem first_menu_items[NUM_MENU_ITEMS];
+
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static int secondCount = 0; //Keeps track of how many seconds since user has moved
@@ -17,7 +23,61 @@ static int x_accel = 0, y_accel = 0, z_accel = 0; //Keeps track of most recent a
 static int x_prev = 0, y_prev = 0, z_prev = 0;    //2nd most recent accelerometer reading
 static int sleep_threshold[4] = {5, 10, 20, 30};
 
+static void main_window_load(Window *window);
+static void menu_select_callback(int index, void *ctx);
+static void main_window_unload(Window *window);
 
+
+
+
+// This initializes the menu upon window load
+static void menu_window_load(Window *window) {
+  // We'll have to load the icon before we can use it
+  //menu_icon_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MENU_ICON1);
+
+  // Although we already defined NUM_FIRST_MENU_ITEMS, you can define
+  // an int as such to easily change the order of menu items later
+  int num_a_items = 0;
+
+  // This is an example of how you'd set a simple menu item
+  first_menu_items[num_a_items++] = (SimpleMenuItem){
+    // You should give each menu item a title and callback
+    .title = "Stay Awake!",
+    .callback = menu_select_callback,
+  };
+  // The menu items appear in the order saved in the menu items array
+  first_menu_items[num_a_items++] = (SimpleMenuItem){
+    .title = "Add a class",
+    // You can also give menu items a subtitle
+    .callback = menu_select_callback,
+  };
+
+  // Bind the menu items to the corresponding menu sections
+  menu_sections[0] = (SimpleMenuSection){
+    .num_items = NUM_MENU_ITEMS,
+    .items = first_menu_items,
+  };
+
+
+  // Now we prepare to initialize the simple menu layer
+  // We need the bounds to specify the simple menu layer's viewport size
+  // In this case, it'll be the same as the window's
+  Layer *menu_window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_frame(menu_window_layer);
+
+  // Initialize the simple menu layer
+  simple_menu_layer = simple_menu_layer_create(bounds, window, menu_sections, NUM_MENU_SECTIONS, NULL);
+
+  // Add it to the window for display
+  layer_add_child(menu_window_layer, simple_menu_layer_get_layer(simple_menu_layer));
+}
+
+void menu_window_unload(Window *window) {
+  simple_menu_layer_destroy(simple_menu_layer);
+
+  // Cleanup the menu icon
+  //gbitmap_destroy(menu_icon_image);
+}
 
 //Tracks whether the watch is moving or not
 //Sensitivity should be updated after testing
@@ -51,6 +111,30 @@ enum App_State get_state(int seconds){
     return level1;
   }
   else return awake;
+}
+
+static void menu_select_callback(int index, void *ctx) {
+  if (index == 0){
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "NOW WE WILL BEGIN");
+    //start the new window and push it 
+    s_main_window = window_create();
+  
+    window_set_window_handlers(s_main_window, (WindowHandlers) {
+      .load = main_window_load,
+      .unload = main_window_unload
+    });
+    
+    window_stack_push(s_main_window, true);
+    
+  }
+  else {
+    
+  }
+  
+  /*// Here we just change the subtitle to a literal string
+  first_menu_items[index].subtitle = "You've hit select here!";
+  // Mark the layer to be updated
+  layer_mark_dirty(simple_menu_layer_get_layer(simple_menu_layer));*/
 }
 
 static void data_handler(AccelData *data, uint32_t num_samples) {
@@ -139,6 +223,13 @@ static void click_config_provider(void *context) {
 
 static void main_window_load(Window *window) {
   
+  window_set_click_config_provider(window, click_config_provider);
+
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler); //Sets the pebble to call the handler every second
+  
+  int num_samples = 10;
+  accel_data_service_subscribe(num_samples, data_handler);
+  accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
   //Sets up the text layer in a rectangle
   s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
   
@@ -157,10 +248,22 @@ static void main_window_load(Window *window) {
 
 static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer); //Destroys the layer to free up memory
+  window_destroy(s_main_window);
 }
 
 static void init() {
-  tick_timer_service_subscribe(SECOND_UNIT, tick_handler); //Sets the pebble to call the handler every second
+  s_menu_window = window_create();
+
+  // Setup the window handlers
+  window_set_window_handlers(s_menu_window, (WindowHandlers) {
+    .load = menu_window_load,
+    .unload = menu_window_unload,
+  });
+
+  window_stack_push(s_menu_window, true /* Animated */);
+ 
+  
+  //tick_timer_service_subscribe(SECOND_UNIT, tick_handler); //Sets the pebble to call the handler every second
   //window_set_click_config_provider(s_main_window, click_config_provider);
   
   /*Sets up the accelerometer
@@ -168,6 +271,7 @@ static void init() {
     -Gives the data to the data_handler function
     -Sets sampling rate to 10Hz
   */
+  /*
   int num_samples = 10;
   accel_data_service_subscribe(num_samples, data_handler);
   accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
@@ -181,11 +285,12 @@ static void init() {
   
   window_set_click_config_provider(s_main_window, click_config_provider);
   
-  window_stack_push(s_main_window, true);
+  window_stack_push(s_main_window, true);*/
 }
 
 static void deinit() {
-  window_destroy(s_main_window);
+  window_destroy(s_menu_window);
+  //window_destroy(s_main_window);
 }
 
 int main(void) {

02617ad4921de49b7b9c269f89a8b8c3f5cec6af
ethanweinberger <ethanweinberger@gmail.com>
2015-01-17 01:19:13 -0500

    Movement measuring implemented

--- src/main.c
+++ src/main.c
@@ -3,11 +3,59 @@ static Window *s_main_window;
 static TextLayer *s_time_layer;
 static int secondCount = 0;
 static int x_accel = 0, y_accel = 0, z_accel = 0;
-static bool asleep = false;
+static int x_prev = 0, y_prev = 0, z_prev = 0;
+static bool buzz = false;
 
+//Tracks whether the watch is moving or not
+//Sensitivity should be updated after testing
+bool moving() { 
+  
+  //Checks to see if watch has moved by 50 units on any axis
+  if ((x_accel >= x_prev + 50) || (x_accel <= x_prev - 50) ||
+        (y_accel >= y_prev + 50) || (y_accel <= y_prev - 50) ||
+        (z_accel >= z_prev + 50) || (z_accel <= z_prev - 50)) {
+    //If yes, we know the watch was moving
+    return true;
+  }
+  else {
+    //If not, it was stationary
+    return false;
+  }
+
+}
+
+bool keepBuzzing() {
+  //Checks to see if watch has moved by 2000 units on any axis (user flicks after being woken up)
+  if ((x_accel >= x_prev + 1000) || (x_accel <= x_prev - 1000) ||
+        (y_accel >= y_prev + 1000) || (y_accel <= y_prev - 1000) ||
+        (z_accel >= z_prev + 1000) || (z_accel <= z_prev - 1000)) {
+    //If yes, no need to keep buzzing
+    return false;
+  }
+  else {
+    //If no, keep going
+    return true;
+  }
+}
 
 static void data_handler(AccelData *data, uint32_t num_samples) {
   // Long lived buffer
+  
+  if (x_accel == 0 && y_accel == 0 && z_accel == 0) {
+    x_accel = data[0].x;    //If this is the first recording, then just record the first values
+    y_accel = data[0].y;
+    z_accel = data[0].z;
+  }
+  else {
+    
+    x_prev = x_accel;    //Holding these values to compare to new ones in the timeticker
+    y_prev = y_accel;
+    z_prev = z_accel;
+    
+    x_accel = data[0].x;
+    y_accel = data[0].y;
+    z_accel = data[0].z;
+  }
   
   x_accel = data[0].x;
   y_accel = data[0].y;
@@ -15,25 +63,58 @@ static void data_handler(AccelData *data, uint32_t num_samples) {
   
 }
 
+static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(s_time_layer, "Up pressed!");
+}
+
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(s_time_layer, "Select pressed!");
+}
+
+static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(s_time_layer, "Down pressed!");
+}
+
+static void click_config_provider(void *context) {
+  // Register the ClickHandlers
+  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+}
+
+
 //Calls the update function every x seconds based on the units_changed variable
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   
   static char s_buffer[128];
   
   //Handling each second
-  if (secondCount < 5) {
-    //If it hasn't been 5 seconds yet, just print the x-acceleration
-    secondCount++;
-    // Compose string of all data
-    snprintf(s_buffer, sizeof(s_buffer), 
-      "X: %d Y: %d Z: %d", x_accel, y_accel, z_accel);
-    text_layer_set_text(s_time_layer, s_buffer);
-  }
+  if (moving()) {
+      text_layer_set_text(s_time_layer, "Good job!");
+      psleep(5000); //Sleeps the watch for 5 seconds
+      secondCount = 0; //Resets the counter for a fresh start
+  } 
   else {
-    //If it has been five seconds, vibrate then reset
-    /*vibes_long_pulse();
-    vibes_short_pulse();*/
-    secondCount = 0;
+    //If it has been five seconds, give a warning
+    if (secondCount == 5) {
+      text_layer_set_text(s_time_layer, "Feeling tired?");
+    }
+    else if (secondCount == 10) {
+      text_layer_set_text(s_time_layer, "Asleep?");
+    }
+    else if (secondCount == 20) {
+      text_layer_set_text(s_time_layer, "Buzzer coming in 10...");
+    }
+    else if (secondCount >= 30) {
+      buzz = true;
+      while (buzz) {
+        vibes_long_pulse();
+        vibes_short_pulse();
+        psleep(1000);
+        buzz = keepBuzzing();
+      }
+    }
+    secondCount++;
   }
 }
 
@@ -60,6 +141,7 @@ static void main_window_unload(Window *window) {
 
 static void init() {
   tick_timer_service_subscribe(SECOND_UNIT, tick_handler); //Sets the pebble to call the handler every second
+  //window_set_click_config_provider(s_main_window, click_config_provider);
   
   /*Sets up the accelerometer
     -Takes one sample (num_samples) every time data is collected

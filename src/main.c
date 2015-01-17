197653a17ef73b8dc74810fa7395488d7f6e3111
ethanweinberger <ethanweinberger@gmail.com>
2015-01-16 23:50:16 -0500

    Added accelerometer data output

--- src/main.c
+++ src/main.c
@@ -2,44 +2,37 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static int secondCount = 0;
+static int x_accel = 0, y_accel = 0, z_accel = 0;
+static bool asleep = false;
 
 
-//Updates the displayed time --> NOT USING NOW
-static void update_time() { 
+static void data_handler(AccelData *data, uint32_t num_samples) {
+  // Long lived buffer
   
-  //Sets up the time container
-  time_t temp = time(NULL);
-  struct tm *tick_time = localtime(&temp);
-  
-  //Buffer to hold the time so that the face can reference it
-  static char buffer[] = "00:00";
-  
-  //Write current hours and minutes
-  if (clock_is_24h_style() == true) {
-    //24h format
-    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
-  }
-  else {
-    //12h format
-    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
-  }
-  
-  text_layer_set_text(s_time_layer, buffer);
+  x_accel = data[0].x;
+  y_accel = data[0].y;
+  z_accel = data[0].z;
   
 }
+
 //Calls the update function every x seconds based on the units_changed variable
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  //update_time();
+  
+  static char s_buffer[128];
   
   //Handling each second
   if (secondCount < 5) {
-    //If it hasn't been 5 seconds yet, do nothing
+    //If it hasn't been 5 seconds yet, just print the x-acceleration
     secondCount++;
+    // Compose string of all data
+    snprintf(s_buffer, sizeof(s_buffer), 
+      "X: %d Y: %d Z: %d", x_accel, y_accel, z_accel);
+    text_layer_set_text(s_time_layer, s_buffer);
   }
   else {
     //If it has been five seconds, vibrate then reset
-    vibes_long_pulse();
-    vibes_short_pulse();
+    /*vibes_long_pulse();
+    vibes_short_pulse();*/
     secondCount = 0;
   }
 }
@@ -52,11 +45,11 @@ static void main_window_load(Window *window) {
   //Sets up the colors and content of the layer
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_text_alignment(s_time_layer, GAlignCenter);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   text_layer_set_text(s_time_layer, "Try to stay awake!");
   
   //Sets up the font for the layer
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
   
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
 }
@@ -67,6 +60,16 @@ static void main_window_unload(Window *window) {
 
 static void init() {
   tick_timer_service_subscribe(SECOND_UNIT, tick_handler); //Sets the pebble to call the handler every second
+  
+  /*Sets up the accelerometer
+    -Takes one sample (num_samples) every time data is collected
+    -Gives the data to the data_handler function
+    -Sets sampling rate to 10Hz
+  */
+  int num_samples = 1;
+  accel_data_service_subscribe(num_samples, data_handler);
+  accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
+  
   s_main_window = window_create();
   
   window_set_window_handlers(s_main_window, (WindowHandlers) {

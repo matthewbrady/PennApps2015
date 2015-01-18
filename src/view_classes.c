0972b0ce10f3cd3b9c05bdaf550939f15700d6df
ethanweinberger <ethanweinberger@gmail.com>
2015-01-18 04:22:18 -0500

    Can View Classes Now

--- src/view_classes.c
+++ src/view_classes.c
@@ -5,55 +5,63 @@
   
 Window *s_main_window;
 
+
+
+
 void view_classes_menu_load(Window *window) {
 
-  
-  numClasses = number_stored();
+  int numClasses = number_stored();
   SimpleMenuItem third_menu_items[numClasses];
 
   int num_a_items = 0;  
   int i;
-  for(i=0; i<SAVE_LIMIT; i++){
+  for(i=0; i<10; i++){
     if(persist_exists(i)){
       interval class;
-      da
-    
-      third_menu_items[num_a_items++] = (SimpleMenuItem){
+      persist_read_data(i, &class, sizeof(class));
       
+      switch (class.day) {
+        case 1:
+          third_menu_items[num_a_items++] = (SimpleMenuItem) {
+            .title = "Sunday"
+          };
+          break;
+        case 2:
+          third_menu_items[num_a_items++] = (SimpleMenuItem) {
+            .title = "Monday"
+          };
+          break;
+        case 3:
+          third_menu_items[num_a_items++] = (SimpleMenuItem) {
+            .title = "Tuesday"
+          };
+          break;
+        case 4:
+          third_menu_items[num_a_items++] = (SimpleMenuItem) {
+            .title = "Wednesday"
+          };
+          break;
+        case 5:
+          third_menu_items[num_a_items++] = (SimpleMenuItem) {
+            .title = "Thursday"
+          };
+          break;
+        case 6:
+          third_menu_items[num_a_items++] = (SimpleMenuItem) {
+            .title = "Friday"
+          };
+          break;
+        case 7:
+          third_menu_items[num_a_items++] = (SimpleMenuItem) {
+            .title = "Saturday"
+          };
+          break;
       }
     }
   }
-  void date_select_menu_load(Window *window) {
-  int num_a_items = 0;
-  
-  second_menu_items[num_a_items++] = (SimpleMenuItem){
-    // You should give each menu item a title and callback
-    .title = "Monday",
-    .callback = menu_select_callback,
-  };
-  second_menu_items[num_a_items++] = (SimpleMenuItem){
-    // You should give each menu item a title and callback
-    .title = "Tuesday",
-    .callback = menu_select_callback,
-  };
-  second_menu_items[num_a_items++] = (SimpleMenuItem){
-    // You should give each menu item a title and callback
-    .title = "Wednesday",
-    .callback = menu_select_callback,
-  };
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
   
   menu_sections[0] = (SimpleMenuSection){
-    .num_items = CLASSES_LIST_LENGTH,
+    .num_items = 10,
     .items = third_menu_items,
   };
   Layer *menu_window_layer = window_get_root_layer(window);
